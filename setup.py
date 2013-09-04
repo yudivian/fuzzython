bce0f300586fba131a4db00cf937cf5ca4ffab90
Yudivian Almeida Cruz <yudy@uh.cu>
2013-09-04 18:28:28 -0400

    Reaplacing README.txt for README

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
 	url='https://github.com/yudivian/fuzzython',
 	license='LICENSE.txt',
 	description='Fuzzy Logic and Fuzzy Inference Python 3 Library',
-	long_description=open('README.txt').read(),
+	long_description=open('README').read(),
 	keywords='Fuzzy Logic Library',
 ) 
