acc06553fefb885233ab99dca57a0a3555c92c3d
Yudivián Almeida Cruz <yudy@uh.cu>
2013-09-04 17:44:18 -0400

    Adding the project github url

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
 	author='Medialab-UH',
 	author_email='yudy@uh.cu',
 	packages= find_packages(),
-	url='',
+	url='https://github.com/yudivian/fuzzython',
 	license='LICENSE.txt',
 	description='Fuzzy Logic and Fuzzy Inference Python 3 Library',
 	long_description=open('README.txt').read(),
